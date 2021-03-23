ac6595ac324f6b90fefb12cfb2a538c6842a1a7f
shamnad-sherief <sshamnad03@gmail.com>
2021-03-23 13:24:00 +0530

    cust edited

--- Customer/buynow.php
+++ Customer/buynow.php
@@ -1,19 +1,105 @@
+
 <?php 
-@session_start();
-require_once("../connectionclass.php");
-$obj=new connectionclass() ; 
-$email_id=$_SESSION['email'] ; 
-$package_id=$_GET['package_id'] ; 
-$date=date("Y-m-d") ; 
-$qry="insert into advertisement_request(email_id,package_id,date)values('$email_id','$package_id','$date')"; 
- $data=$obj->Manipulation($qry); 
- if($data['status']=='true')
-				{
-				   
-					 echo $obj->alert("Successfully added","dashboard.php");
-			        }
-				else
-				{
-				     echo $obj->alert('Registration process failed,Try again',"dashboard.php");
-				} 
+require_once("header.php");
+
+$username=$_SESSION['email'] ;
 ?>
+<section class="wrapper">
+<div class="table-agile-info">
+<div class="row">
+<div class="col-lg-12">
+<section class="panel">
+<?php
+$package_id=$_GET['package_id'] ; 
+require_once('../connectionclass.php');
+$obj=new ConnectionClass();
+
+$data= $obj->GetTable("select * from packages where package_id=$package_id");
+   foreach($data as $row){
+	   
+	$amount= $row["amount"];
+	   
+	   
+?>
+
+<!-- services -->
+<section class="panel  ">
+	<div class="panel-body  ">
+		<h2 class="heading mb-5">Package  <strong> We provide </strong> <hr></h2>
+		<div class="row">
+			<div class="col-md-6  "  >
+				 	 
+						<h3><?php echo  $row["title"]     ?></h3>
+						 
+				 
+					
+					 
+						<p class="mt-2"><?php echo  $row["description"]     ?></p>
+			</div>
+					 
+				  
+			 
+			<div class="col-md-4 text-left">
+				 <div class="panel-body" style="border:2px solid red;border-radius:5px">
+                            <div class="position-center">
+                               <form class="form-horizontal" method="post"  action="savepayment.php">
+ 
+ <div class="form-group">
+                                    <label for="exampleInputAmount1">Amount</label>
+                                    <input type="text" class="form-control" required id="exampleInputAmount" placeholder="Amount" name="amount" value="<?php echo  $amount  ?>" readonly  >
+                                </div>
+                               <div class="form-group">
+                                    <label for="exampleInputCardNumber1">CardNumber</label>
+                                    <input type="number" class="form-control" required id="exampleInputCardNumber1" placeholder="CardNumber" name="cardno">
+                                </div>
+								<div class="form-group">
+                                    <label for="exampleInputExpiryear1">Expiryear</label>
+                                    <input type="number" class="form-control" required id="exampleInputExpiryear1" placeholder="Expiryear" name="expiryyear">
+                                </div>
+								<div class="form-group">
+                                    <label for="exampleInputExpirMonth">Expiry Month</label>
+                                    <input type="text" class="form-control" required id="exampleInputExpirMonth1" placeholder="ExpirMonth" name="expirymonth">
+                                </div>
+								<div class="form-group">
+                                    <label for="exampleInputCwno">Cvvno</label>
+                                    <input type="number" class="form-control" required id="exampleInputCwno" placeholder="CVV" name="cvv">
+                                 <input type="hidden" class="form-control" id="exampleInputCwno" placeholder="package_id" name="package_id" value="<?php echo  $row["package_id"] ?>">
+                                </div>
+ 
+
+
+     <div class="form-group">
+	 <div class="col-sm-12">
+	 
+	 <input type="hidden" name="rid"   id="rid"   />
+	 
+           <button type="submit" class="btn btn-danger pull-right">PAY</button>
+				</div>
+</div>
+
+					</form>
+ 
+
+
+							   </div>
+											 
+						</div>
+					 
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- //services -->
+
+<?php
+   }
+?>
+</div>
+</div>
+</div>
+</section>
+<?php
+require_once("footer.php");
+?> 
---
+++ Customer/savepayment.php
@@ -0,0 +1,48 @@
+<?php
+@session_start();
+require_once("../connectionclass.php");
+ 
+ 
+$obj = new ConnectionClass();
+   $amount=$_POST['amount'];
+   $cardno=$_POST['cardno'];
+   $expiryyear=$_POST['expiryyear'];
+   $expirymonth=$_POST['expirymonth'];
+  // $reg_place=$_POST['place'];
+   $cvv=$_POST['cvv'];
+ 
+$expiry=  $expirymonth."/".$expiryyear;
+$email_id= $_SESSION["email"];
+$package_id=$_POST['package_id'] ;
+$date=date("Y-m-d") ; 
+$checkbank="select amount from bank where card_no='$cardno' and cvv='$cvv' and expiry='$expiry'";
+$resultcount=$obj->GetSingleData($checkbank);
+if($resultcount=="")
+{
+ 
+  echo $obj->alert("Card Not exists","buynow.php?package_id=".$package_id);  
+}
+else if($amount>$resultcount){
+	echo $obj->alert("Insufficient Balance","buynow.php?package_id=".$package_id);
+}else
+{
+	$sql="insert into advertisement_request(email_id,package_id,date)values('$email_id','$package_id','$date')"; 
+	$exe=$obj->Manipulation($sql);
+	//var_dump($exe);
+	if($exe['status']=='true')
+	{
+		 
+		 
+		 echo $obj->alert("Payment Successfully Completed..","dashboard.php");      	
+		 
+	}
+	else
+	{
+		 
+
+		 echo $obj->alert("Something Went Wrong","buynow.php?package_id=".$package_id);
+	}
+
+
+}
+?>
